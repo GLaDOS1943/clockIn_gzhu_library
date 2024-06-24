e4aad89c5af8284091faa2c76fa3921c59b493c8
GLaDOS1943 <107766165+GLaDOS1943@users.noreply.github.com>
2024-06-24 19:04:17 +0800

    Update clockIn_lib.py

--- clockIn_lib.py
+++ clockIn_lib.py
@@ -183,7 +183,7 @@ class clockIn():
         logger.info('step3 准备进行图书馆预定座位操作')
         logger.info('标题: ' + self.driver.title)
 
-        cookie = self.get_cookie()
+        cookie = self.driver.get_cookie()
 
         if cookie == '':
             logger.info('没找到cookie')
